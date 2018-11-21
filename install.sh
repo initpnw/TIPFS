25e0ba622d5a851bea164a489f4a80994027027c
Lee Hundley <lee.hundley@gmail.com>
2018-11-20 16:15:58 -0800

    Updates

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 DumpVars() {
   echo "Cannot install, dump follows"
   echo
-  echo "PREFIX=$PREFIX"
   echo "DEST_PATH=$DEST_PATH"
   echo "BIN_PATH=$BIN_PATH"
   echo
@@ -12,12 +11,10 @@ DumpVars() {
   echo
   echo "GO_URL=$GO_URL"
   echo "IPFS_URL=$IPFS_URL"
+  echo "TIPFS_URL=$TIPFS_URL"
   echo
   echo "GO_FILE=$GO_FILE"
   echo "IPFS_FILE=$IPFS_FILE"
-  echo
-  echo "GO_URL=$GO_URL"
-  echo "IPFS_URL=$IPFS_URL"
   echo
   echo "IPFS_PORT=$IPFS_PORT"
   echo "DNS_ENDPOINT=$DNS_ENDPOINT"
@@ -26,7 +23,7 @@ DumpVars() {
   echo "BOOTSTRAP_ENDPOINT=$BOOTSTRAP_ENDPOINT"
   echo "SWARMKEY_ENDPOINT=$SWARMKEY_ENDPOINT"
   echo "IPFSV_ENDPOINT=$IPFSV_ENDPOINT"
-
+  echo
   echo "BOOTSTRAP=$BOOTSTRAP"
   echo "SWARMKEY=$SWARMKEY"
   echo "IPFSV=$IPFSV"
@@ -41,7 +38,6 @@ GLOBAL=false
 GO_PATH=/usr/local
 TMP_PATH=/tmp
 LOG_PATH=$DEST_PATH/log
-TIPFS_PATH=$DEST_PATH/bin
 
 IPFS_PORT=4001
 NETWORK=test
@@ -77,10 +73,14 @@ done
 
 
 BOOTSTRAP=$(dig +noall +answer TXT $BOOTSTRAP_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \")
-SWARMKEY=$(dig +noall +answer TXT $SWARMKEY_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \" | base64 -d )
+SWARMKEY=$(dig +noall +answer TXT $SWARMKEY_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \" | base64 -d | tr -d "\n" )
 GOLANGV=$(dig +noall +answer TXT $GOLANGV_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \" | base64 -d )
 IPFSV=$(dig +noall +answer TXT $IPFSV_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \" | base64 -d )
 #TIPFSV=$(dig +noall +answer TXT $TIPFSV_ENDPOINT.$NETWORK.$DNS_ENDPOINT | cut -f2 | tr -d \" | base64 -d )
+
+# Check sanity of stuff built from TXT recofds here:
+# Forcing $GOLANGV until TXT record is fixed
+GOLANGV="go1.11.linux-amd64.tar.gz"
 
 GO_URL="https://dl.google.com/go/$GOLANGV"
 IPFS_URL="https://dist.ipfs.io/go-ipfs/$IPFSV"
@@ -89,21 +89,34 @@ TIPFS_URL='git@github.com:Telos-Foundation/TIPFS.git'
 
 GO_FILE=`basename $GO_URL`
 IPFS_FILE=`basename $IPFS_URL`
-TIPFS_FILE=`basename $TIPFS_URL`
+# TIPFS_FILE=`basename $TIPFS_URL`
 
 # Debug
 if [ "$DEBUGME" ] ; then
   DumpVars
 fi
 
+# Setup tmp space
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
+
 # Download 
-wget $GO_URL
-wget $IPFS_URL
-# git clone $TIPFS_URL
+if ! wget $GO_URL ; then
+  echo "Cannot download Go"
+  exit 1
+fi
+if ! wget $IPFS_URL ; then
+  echo "Cannot download ipfs"
+  exit 1
+fi
+if ! git clone $TIPFS_URL ; then
+  echo "Cannot download TIPFS"
+  exit 1
+fi
 
 # Extract
 
-Echo "Installing Go Language. Sudo access needed"
+echo "Installing Go Language to $GO_PATH. Sudo access needed"
 sudo tar -C $GO_PATH -xzf $GO_FILE
 tar -C $TMP_PATH -xzf $IPFS_FILE
 # tar -C $TMP_PATH -xzf $TIPFS_FILE
@@ -112,12 +125,15 @@ tar -C $TMP_PATH -xzf $IPFS_FILE
 
 # Go installs on previous step, extract only
 
-# cd $IPFS_PATH
+# IPFS
 
+echo "Installing TIPFSto $BIN_PATH"
+cp -r TIPFS/* $BIN_PATH
+
+echo "Installing ipfs to $BIN_PATH"
 if mv "$TMP_PATH/go-ipfs/ipfs $BIN_PATH/ipfs" 2> /dev/null; then
 
   echo "Moved ipfs to $HOME/bin/ipfs"
-  exit 0
 else
   if [ -d "$BIN_PATH" -a ! -w "$BIN_PATH" ]; then
     is_write_perm_missing=1
@@ -134,3 +150,10 @@ if [ -n "$is_write_perm_missing" ]; then
   exit 1
 fi
 
+# Clean up tmp
+cd -
+rm -rf $TMP_DIR
+
+# Add stuff to bash_aliases
+
+echo 'export GOPATH=$HOME/.go' >> ~/.bash_aliases
